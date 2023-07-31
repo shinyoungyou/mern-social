375ad7c82985430a8e1178d1f364b3469b595f9a
AustynYou <austyn.you@gmail.com>
2023-07-31 16:17:27 -0700

    import mock data

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
